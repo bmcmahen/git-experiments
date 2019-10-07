4c5f3e5324c4a7555a00db868a1545ac2b989394
bmcmahen <ben.mcmahen@gmail.com>
2019-10-07 06:25:58 -0700

    color tweaks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header add-class">
         <p>
           Edit <code>src/App.js</code> and save them.
         </p>
