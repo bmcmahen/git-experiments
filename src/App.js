efa47f08024e4365aaaf5f1e2639b09df1efa15e
bmcmahen <ben.mcmahen@gmail.com>
2019-10-07 06:28:26 -0700

    add eh message

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React is cool
+          Learn React is cool eh?
         </a>
       </header>
     </div>
